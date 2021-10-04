27938aa756510411af8119aa7f68e9e8250252e6
maxim <maxim@192.168.115.158>
2021-10-04 18:56:53 +0200

    Actualisation

--- convertisseur_Corlay_Nobili/src/convertisseur_corlay_nobili/Convertisseur_Corlay_Nobili.java
+++ convertisseur_Corlay_Nobili/src/convertisseur_corlay_nobili/Convertisseur_Corlay_Nobili.java
@@ -4,6 +4,8 @@
  */
 package convertisseur_corlay_nobili;
 
+import java.util.Scanner;
+
 /**
  *
  * @author maxim
@@ -14,7 +16,63 @@ public class Convertisseur_Corlay_Nobili {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc=new Scanner(System.in) ;
+        float temp;
+	int conversion;
+        System.out.println("Bonjour, saisissez une valeur :");
+        temp = sc.nextFloat();
+        System.out.println("Saisissez la conversion que vous souhaitez effectuer :\n1) De Celcius vers Kelvin\n2) De Kelvin vers Celcius\n3) De Farenheit Vers Celcius\n4) De Celcius Vers Farenheit\n5) De Farenheit Vers Kelvin\n6) De Kelvin Vers Farenheit");
+        conversion = sc.nextInt();
+        if (conversion==1) {
+            System.out.println(temp +" degrés Celcius est égal à "+CelciusVersKelvin (temp)+" degrés Kelvin");
+	}
+        if (conversion==2) {
+            System.out.println(temp +" degrés Kelvin est égal à "+KelvinVersCelcius (temp)+" degrés Celcius");
+        }
+        if (conversion==3) {
+            System.out.println(temp +" degrés Farenheit est égal à "+FarenheitVersCelcius (temp)+" degrés Celcius");
+        }
+        if (conversion==4) {
+            System.out.println(temp +" degrés Celcius est égal à "+ CelsiusVersFarenheit (temp)+" degrés Farenheit");
+        }
+        if (conversion==5) {
+            System.out.println(temp +" degrés Farenheit est égal à "+FarenheitVersKelvin (temp)+" degrés Kelvin");
+        }
+        if (conversion==6) {
+            System.out.println(temp +" degrés Kelvin est égal à "+KelvinVersFarenheit (temp)+" degrés Farenheit");
+        }
+
+
     }
-    
+    public static float CelciusVersKelvin ( float tempC) {
+        float tempK ;
+        tempK = tempC + 273,15 ;
+        return (tempK) ;
+        
+    }
+    public static float KelvinVersCelcius ( float tempK) {
+        float tempC;
+        tempC = tempK-273,15 ;
+        return (tempC);
+    }
+    public static float FarenheitVersCelcius ( float tempC) {
+        float tempC;
+        tempC = tempF/1,8 - 32/1,8 ;
+        return (tempC);
+    }
+    public static float CelsiusVersFarenheit ( float tempC) {
+        float tempF;
+        tempF = tempC*1,8 + 32;
+        return (tempF);
+    }
+    public static float KelvinVersFarenheit ( float tempK) {
+        float tempF;
+        tempF = (tempK - 273,15)*1,8 + 32 ;
+        return (tempF);
+    }
+    public static float FarenheitVersKelvin ( float tempF) {
+        float tempK;
+        tempK = (tempF - 32)/1,8 + 273,15;
+        return (tempK);
+    }
 }
