7352331d468422ec7cb7c9d5e49e595d7c36bade
maxim <maxim@192.168.115.158>
2021-11-12 14:24:38 +0100

    finition classe jeton

---
+++ SP4_console_NOBILI_CORLAY/src/sp4_console_nobili_corlay/Jeton.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_nobili_corlay;
+
+/**
+ *
+ * @author maxim
+ */
+public class Jeton { 
+    String Couleur;
+    
+    public String Jeton(String CouleurJoueur){
+        Couleur = CouleurJoueur ;
+    }
+    public String lireCouleur(String CouleurJoueur){
+        return CouleurJoueur ;
+    }
+    
+}
+
