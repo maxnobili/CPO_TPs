38dad9d26aa0cc1d9058e11e2270acb4f17f3c1e
maxim <maxim@192.168.115.158>
2021-11-12 15:30:36 +0100

    debut

---
+++ SP4_console_NOBILI_CORLAY/src/sp4_console_nobili_corlay/Grille.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_nobili_corlay;
+
+/**
+ *
+ * @author maxim
+ */
+public class Grille { 
+    Cellule [][] CelluleJeu = new Cellule [6][7];
+    
+    
+    public Grille(){
+        CelluleJeu = null;
+    }
+    
+    public boolean ajouterJetonDansColonne(Jeton Jeton,int ColonneJoueur){
+        for (i=0; i<6; i++){
+            if jetonJoueur == true{
+        }
+    }
+    
+    public boolean etreRemplie(){
+        
+    }
+    
+    public void viderGrille(){
+        
+    }
+    
+    public void afficherGrilleSurConsole(){
+        
+    }
+    
+    public boolean celluleOccupee(int,int){
+        
+    }
+    
+    public String lireCouleurDuJeton(){
+        
+    }
+    
+    public boolean etreGagnantePourJoueur(){
+        
+    }
+    
+    public void tasserGrille(int){
+        
+    }
+    
+    public boolean colonneRemplie(int){
+        
+    }
+    
+    public boolean placerDesintegrateur(int,int){
+        
+    }
+    
+    public boolean placerTrouNoir(int,int){
+        
+    }
+    
+    public boolean supprimerJeton(int,int){
+        
+    }
+    
+    
+}
+
