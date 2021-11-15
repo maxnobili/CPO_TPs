e361796af421df49083d7e03ae0a7d7d1b01b21e
maxim <maxim@192.168.115.158>
2021-11-15 14:12:16 +0100

    debut classe partie

---
+++ SP4_console_NOBILI_CORLAY/src/sp4_console_nobili_corlay/Partie.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_nobili_corlay;
+
+/**
+ *
+ * @author maxim
+ */
+public class Partie { 
+    int [] ListeJoueurs = new int [2];
+    Joueur joueurCourant;
+    Grille grilleJeu;
+   
+    public Partie(Joueur,Joueur){
+       
+    }
+   
+    public void initialiserPartie(){
+       
+    }
+   
+    public void debuterPartie(){
+       
+    }
+   
+    public void attribuerCouleursAuxJoueurs(){
+       
+    }
+   
+}
