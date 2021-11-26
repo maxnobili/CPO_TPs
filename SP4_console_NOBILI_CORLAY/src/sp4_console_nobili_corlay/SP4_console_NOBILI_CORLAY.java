a909462f631bd06f8548e905ff0916c069f85ad1
maxim <maxim@192.168.114.122>
2021-11-26 13:08:12 +0100

    finition desintegrateur

--- SP4_console_NOBILI_CORLAY/src/sp4_console_nobili_corlay/SP4_console_NOBILI_CORLAY.java
+++ SP4_console_NOBILI_CORLAY/src/sp4_console_nobili_corlay/SP4_console_NOBILI_CORLAY.java
@@ -4,17 +4,46 @@
  */
 package sp4_console_nobili_corlay;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author maxim
  */
 public class SP4_console_NOBILI_CORLAY {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        Scanner scannermain = new Scanner(System.in);
+        System.out.println("Bonjour vous avez decidez de commencer une partie de SUPER Puissance 4");
+        System.out.println("Entrez le nom d'un des joueurs s'il vous plait:");
+        String nomfct1 = scannermain.nextLine();
+        Joueur Nomj1 = new Joueur(nomfct1);
+        System.out.println("Entrez le nom d'un des joueurs s'il vous plait:");
+        String nomfct2 = scannermain.nextLine();
+        Joueur Nomj2 = new Joueur(nomfct2);
+        Partie partie =  new Partie(Nomj1 , Nomj2);
+        partie.initialiserPartie();
+        partie.debuterPartie();
+        
+        
+        
+        Jeton j = new Jeton("rouge");
+        String c = j.lireCouleur();
+        System.out.println("la couleur est " + c);
+        Cellule cell = new Cellule();
+        c = cell.lireCouleurDuJeton();
+        System.out.println(c);
+
+        
+        
+        
+        
         // TODO code application logic here
     }
     
 }
+
